955022df5fbed271f09408f89fbf41bf94c13921
Otto Matiss <matiss.aprups@gmail.com>
2016-09-29 10:13:13 +0200

    updated string, style and Main Activity with button and used Bundle

---
+++ app/src/main/java/com/softdesign/devintensive/utils/ConstantManager.java
@@ -0,0 +1,10 @@
+package com.softdesign.devintensive.utils;
+
+
+
+//those are static final fields (those are constants) because we are
+// working in the interface. No need to declare them static final.
+public interface ConstantManager {
+    String TAG_PREFIX = "DEV ";
+    String COLOR_MODE_KEY = "COLOR_MODE_KEY";
+}
